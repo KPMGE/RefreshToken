56ad9d37dab54118e34edc1d4c3c9d75337dcabf
KPMGE <jesuskevin254@gmail.com>
2022-02-12 16:15:14 -0300

    add RefreshToken feature :sparkles:

---
+++ src/useCases/RefreshToken/RefreshTokenController.ts
@@ -0,0 +1,8 @@
+
+import { Request, Response } from 'express'
+export class RefreshTokenController {
+  async handle(request: Request, response: Response) {
+
+  }
+}
+
---
+++ src/useCases/RefreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,3 @@
+export class RefreshTokenUseCase {
+  async execute() {}
+}
