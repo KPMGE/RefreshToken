6fbc6105e834362639318e2d7817154db04d4678
KPMGE <jesuskevin254@gmail.com>
2022-02-12 10:38:36 -0300

    Add interface for UsersRepository :sparkles:

---
+++ src/repositories/IUsersRepository.ts
@@ -0,0 +1,7 @@
+import { User } from "../entities/User";
+
+export interface IUsersRepositoty {
+  save(user: User): Promise<User>;
+  findUserById(id: string): Promise<User>;
+  findUserByUserName(user_name: string): Promise<User>;
+}
