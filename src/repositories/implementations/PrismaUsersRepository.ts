193e5d6b069ac18bba89bca926fa6454621588e2
KPMGE <jesuskevin254@gmail.com>
2022-02-12 10:38:06 -0300

    Add prisma implementation for IUsersRepository :sparkles:

---
+++ src/repositories/implementations/PrismaUsersRepository.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { User } from "../../entities/User";
+import { IUsersRepositoty } from "../IUsersRepository";
+
+const client = new PrismaClient();
+
+export class PrismaUsersRepository implements IUsersRepositoty {
+  async save(user: User): Promise<User> {
+    const savedUser = await client.user.create({
+      data: user,
+    });
+
+    return savedUser;
+  }
+
+  async findUserById(id: string): Promise<User> {
+    const foundUser = await client.user.findFirst({
+      where: { id },
+    });
+
+    return foundUser;
+  }
+
+  async findUserByUserName(user_name: string): Promise<User> {
+    const foundUser = await client.user.findFirst({
+      where: { user_name },
+    });
+
+    return foundUser;
+  }
+}
