b4f10c6e84fd2130dcf6765e663172af4bf86e19
KPMGE <jesuskevin254@gmail.com>
2022-02-12 16:13:40 -0300

    Add test route :sparkles:

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { authenticateUserController } from "./useCases/AuthenticateUser";
 import { createUserController } from "./useCases/CreateUser";
 
@@ -10,6 +11,10 @@ routes.post("/login", (request, response) => {
 
 routes.post("/users/new", (request, response) => {
   createUserController.handle(request, response);
+});
+
+routes.get("/test", ensureAuthenticated, (req, res) => {
+  return res.json(["Kevin", "Luana", "Karol", "Laura"]);
 });
 
 export { routes };
