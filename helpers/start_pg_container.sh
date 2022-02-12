6e66be27f700950cff6eb59eb5e2653637c4886f
KPMGE <jesuskevin254@gmail.com>
2022-02-12 10:40:11 -0300

    Add script for initializing postgres container with docker :sparkles:

--- helpers/start_pg_container.sh
+++ helpers/start_pg_container.sh
@@ -3,3 +3,4 @@
 # Runs a container with postgres with the password 'root' and exposes the port 5432
 echo "Starting postgres container running on port 5432..."
 sudo docker run -it -d --rm -p 5432:5432 -e POSTGRES_PASSWORD=root --name pg_container postgres:alpine
+echo "All done!"
