f013a8112abf566c049b8147057fc181414f5704
KPMGE <jesuskevin254@gmail.com>
2022-02-13 20:59:42 -0300

    Add routes to get user by id and get all uses :sparkles:

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { authenticateUserController } from "./useCases/AuthenticateUser";
 import { createUserController } from "./useCases/CreateUser";
+import { getAllUsersController } from "./useCases/GetAllUsers";
+import { getUserByIdController } from "./useCases/GetUserById";
 import { refreshTokenController } from "./useCases/RefreshToken";
 
 const routes = Router();
@@ -10,16 +12,20 @@ routes.post("/login", (request, response) => {
   authenticateUserController.handle(request, response);
 });
 
+routes.get("/users", ensureAuthenticated, (request, response) => {
+  getAllUsersController.handle(request, response);
+});
+
+routes.get("/users/:user_id", ensureAuthenticated, (request, response) => {
+  getUserByIdController.handle(request, response);
+});
+
 routes.post("/users/new", (request, response) => {
   createUserController.handle(request, response);
 });
 
 routes.post("/users/refresh-token", (request, response) => {
   refreshTokenController.handle(request, response);
-});
-
-routes.get("/test", ensureAuthenticated, (req, res) => {
-  return res.json(["Kevin", "Luana", "Karol", "Laura"]);
 });
 
 export { routes };
