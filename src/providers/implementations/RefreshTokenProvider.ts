2725bf44b45de5034cf19384e0ebba15e7a55072
KPMGE <jesuskevin254@gmail.com>
2022-02-13 21:38:35 -0300

    update expiration time of the refresh token to 1 day :sparkles:

--- src/providers/implementations/RefreshTokenProvider.ts
+++ src/providers/implementations/RefreshTokenProvider.ts
@@ -7,7 +7,7 @@ export class RefreshTokenProvider implements IRefreshTokenProvider {
   constructor(private refreshTokenRepository: IRefreshTokenRepository) {}
 
   async generate(user_id: string): Promise<RefreshToken> {
-    const expires_in = dayjs().add(15, "day").unix();
+    const expires_in = dayjs().add(1, "day").unix();
 
     const newRefreshToken = new RefreshToken({ user_id, expires_in });
     await this.refreshTokenRepository.save(newRefreshToken);
