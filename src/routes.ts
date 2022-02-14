9391599053983578fb13d274aec3393e7de215c8
KPMGE <jesuskevin254@gmail.com>
2022-02-13 22:09:15 -0300

    Update routes :sparkles:

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { authenticateUserController } from "./useCases/AuthenticateUser";
 import { createUserController } from "./useCases/CreateUser";
+import { deleteUserController } from "./useCases/DeleteUser";
 import { getAllUsersController } from "./useCases/GetAllUsers";
 import { getUserByIdController } from "./useCases/GetUserById";
 import { refreshTokenController } from "./useCases/RefreshToken";
@@ -12,12 +13,12 @@ routes.post("/login", (request, response) => {
   authenticateUserController.handle(request, response);
 });
 
-routes.get("/users", ensureAuthenticated, (request, response) => {
-  getAllUsersController.handle(request, response);
-});
-
 routes.get("/users/:user_id", ensureAuthenticated, (request, response) => {
   getUserByIdController.handle(request, response);
+});
+
+routes.get("/users", ensureAuthenticated, (request, response) => {
+  getAllUsersController.handle(request, response);
 });
 
 routes.post("/users/new", (request, response) => {
@@ -26,6 +27,10 @@ routes.post("/users/new", (request, response) => {
 
 routes.post("/users/refresh-token", (request, response) => {
   refreshTokenController.handle(request, response);
+});
+
+routes.delete("/users/delete/:userId", (request, response) => {
+  deleteUserController.handle(request, response);
 });
 
 export { routes };
