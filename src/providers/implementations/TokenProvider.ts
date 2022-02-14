789bbd504c010f163f57e9b50d1160f58d89ec28
KPMGE <jesuskevin254@gmail.com>
2022-02-13 21:37:59 -0300

    Update expiration token time to 1min :sparkles:

--- src/providers/implementations/TokenProvider.ts
+++ src/providers/implementations/TokenProvider.ts
@@ -5,7 +5,7 @@ export class TokenProvider implements ITokenProvider {
   generate(user_id: string): string {
     const token = sign({}, "secret key", {
       subject: user_id,
-      expiresIn: "20s",
+      expiresIn: "60s",
     });
 
     return token;
